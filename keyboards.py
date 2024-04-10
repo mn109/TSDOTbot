fbc0874398609263ba5d4ac35033e784859987a2
Mikhail Nikitin <mkhl.nktn@gmail.com>
2024-04-10 14:09:47 +0400

    feat: add another YouTube video button

--- keyboards.py
+++ keyboards.py
@@ -28,16 +28,6 @@ vk_public_button = InlineKeyboardButton(
     url="https://vk.com/xxxxxxxxxx"
 )
 
-top_3_button = InlineKeyboardButton(
-    text="Ролик для YouTube-канала (107 сек.)",
-    url="https://youtu.be/my4feD2Cqgg?si=Hch1WC9cR2FfFKRk"
-)
-
-about_us_vk_clip_button = InlineKeyboardButton(
-    text="Клип VK",
-    url="https://vk.ru/clip-78784229_456239033"
-)
-
 social_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [telegram_channel_button],
@@ -50,4 +40,19 @@ watch_keyboard = InlineKeyboardMarkup(
         [top_3_button],
         [about_us_vk_clip_button]
     ]
+)
+
+top_3_button = InlineKeyboardButton(
+    text="Ролик для YouTube-канала (107 сек.)",
+    url="https://youtu.be/my4feD2Cqgg?si=Hch1WC9cR2FfFKRk"
+)
+
+top_10_button = InlineKeyboardButton(
+    text="Ролик для YouTube-канала (12 мин.)",
+    url="https://www.youtube.com/watch?v=tUWfljgFJD8"
+)
+
+about_us_vk_clip_button = InlineKeyboardButton(
+    text="Клип VK (20 сек.)",
+    url="https://vk.ru/clip-78784229_456239033"
 )
